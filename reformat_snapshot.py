603ce6f9c8e691a273d5d1cd0f23d1e6de8da45d
GennadiyProg <64655942+GennadiyProg@users.noreply.github.com>
2024-05-20 01:19:49 +0300

    Update reformat_snapshot.py

--- reformat_snapshot.py
+++ reformat_snapshot.py
@@ -2,7 +2,7 @@ import pandas as pd
 from datetime import datetime
 
 
-# Функция для обработки каждой порции данных
+
 def process_chunk(chunk, agg_data):
     
     for index, row in chunk.iterrows():
@@ -24,25 +24,25 @@ def process_chunk(chunk, agg_data):
         if date > agg_data[wallet]['last']:
             agg_data[wallet]['last'] = date
 
-        # Для безопасности сначала проверьте, что NATIVE_DROP_USD и STARGATE_SWAP_USD могут быть преобразованы в float
+       
         native = row.get('NATIVE_DROP_USD', 0)
         stargate = row.get('STARGATE_SWAP_USD', 0)
         agg_data[wallet]['total'] += float(native or 0) + float(stargate or 0)
 
-# Инициализация словаря для агрегированных данных
+
 aggregated_data = {}
 
-# Чтение файла по частям
+
 chunk_size = 5*(10**6)  # Можно подстроить в зависимости от объёма доступной памяти
 for chunk in pd.read_csv('snapshot.csv', chunksize=chunk_size):
     process_chunk(chunk, aggregated_data)
 print("Reading ended")
 
-# Преобразование агрегированных данных в DataFrame
+
 aggregated_df = pd.DataFrame.from_dict(aggregated_data, orient='index',
                                        columns=['count', 'first', 'last', 'total'])
 print("Agregation ended")
 
 
-# Сохранить агрегированный DataFrame в файл
+
 aggregated_df.to_csv('aggregated_data.csv')
